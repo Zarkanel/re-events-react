ccdd505c37e55359c47b4a1c47b5f235eaf4e818
Mendy Merzel <Mmerzel@gmail.com>
2020-05-01 17:55:36 +0300

    End of section 5

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -7,12 +7,24 @@ class EventForm extends Component {
         date: '',
         city: '',
         venue: '',
-        hostedBy: '',
-
+        hostedBy: ''
     }
+    
+    componentDidMount(){
+        if (this.props.selectedEvent != null){
+            this.setState({
+                ...this.props.selectedEvent
+            })
+        }
+    }
+
     handleFormSubmit = (evt) => {
         evt.preventDefault();
-        this.props.createEvent(this.state);
+        if (this.state.id) {
+            this.props.updateEvent(this.state);
+        } else {
+            this.props.createEvent(this.state);
+        }
     }
 
     handleChange = (evt) => {
